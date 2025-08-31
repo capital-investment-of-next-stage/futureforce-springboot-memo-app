b373d011f47ff3f71231284931ab22dc497bacd8
sato-ay <sato-ay@cin-group.com>
2025-08-31 21:40:33 +0900

    Initial commit

---
+++ src/main/java/com/lesson/memo/model/Memo.java
@@ -0,0 +1,36 @@
+package com.lesson.memo.model;
+
+import java.time.LocalDateTime;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Entity
+@Data
+public class Memo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "タイトルを入力してください")
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @NotBlank(message = "内容を入力してください")
+    @Column(nullable = false, length = 1000)
+    private String content;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime updatedAt;
+}
